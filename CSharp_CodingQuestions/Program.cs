e3716b848ad2144f5ce3affc1f35f3b31845929d
Siva Kovvuri <siva7050@yahoo.com>
2024-12-25 21:12:30 +0530

    committing

---
+++ CSharp_CodingQuestions/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_CodingQuestions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ TwoSum/Program.cs
@@ -0,0 +1,37 @@
+﻿    namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] result=TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine(result[i]);
+            }
+        }
+
+        static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> numDict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (numDict.ContainsKey(complement))
+                {
+                    return new int[] { numDict[complement], i };
+                }
+                if (!numDict.ContainsKey(nums[i]))
+                {
+                    numDict[nums[i]] = i;
+                }
+            }
+
+            throw new ArgumentException("No two sum solution");
+        }
+    }
+
+}
+
